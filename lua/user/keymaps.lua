99e56d0df1ef950146c236d5090a8470b3a3eb29
Tomer Blu <tomer.blushinsky@forter.com>
2022-01-02 15:15:19 +0200

    telescope

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -37,3 +37,8 @@ keymap("n", alt_k, ":resize +2<CR>", opts)
 keymap("n", alt_j, ":resize -2<CR>", opts)
 keymap("n", alt_h, ":vertical resize -2<CR>", opts)
 keymap("n", alt_l, ":vertical resize +2<CR>", opts)
+
+-- Telescope
+-- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
+keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -59,7 +59,8 @@ return packer.startup(function(use)
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
   -- Automatically set up your configuration after cloning packer.nvim
   if PACKER_BOOTSTRAP then
     require("packer").sync()
